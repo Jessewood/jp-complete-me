241ab89bffce675a898f45f2cd52e9e119dac28f
Jesse Packwood <packwoodjustin@gmail.com>
2017-10-25 22:34:07 -0600

    lint corrections

--- tests/Node-test.js
+++ tests/Node-test.js
@@ -1,26 +1,29 @@
 import { expect } from 'chai';
 import Node from '../lib/Node.js';
-import Trie from '../lib/Trie.js';
-
 
 describe('Node test', () => {
 
-  beforeEach(() => {
-    node = new Node()
+  let node;
 
+  beforeEach(() => {
+    node = new Node('');
+  });
 
   it('should be a thing', () => {
-    expect(node).to.exist
-  })
+    expect(node).to.exist;
+  });
 
-  it('should start with null as a default', () => {
-    expect(node.letter).to.equal(null)
-  })
+  it('should start with an empty string as a default', () => {
+    expect(node.letter).to.equal('');
+  });
 
-  it (should take a letter as a parameter and assign it to a node)
+  it('should take a letter as a parameter and assign it to a node', () => {
+    node = new Node('a');
+    expect(node.letter).to.deep.equal('a');
+  });
 
   it('should have no children', () => {
     expect(node.children).to.deep.equal({});
-  })
-})
-})
+  });
+
+});
--- tests/Trie-test.js
+++ tests/Trie-test.js
@@ -1,79 +1,126 @@
 import { expect } from 'chai';
-import Trie  from '../lib/Trie.js'
-import Node from '../lib/Node.js'
+import Trie  from '../lib/Trie.js';
+import Node from '../lib/Node.js';
 import fs from 'fs';
 
-const text = "/usr/share/dict/words"
+const text = "/usr/share/dict/words";
 const dictionary = fs.readFileSync(text).toString().trim().split('\n');
 
+let trie;
+
+beforeEach(() => {
+  trie = new Trie();
+});
 
 describe('Insert', () => {
 
-  it('should be a function', () => {
+  it('should be a method', () => {
     expect(Trie).to.be.a('function');
-  })
+  });
 
-  it('should be able to insert words into the trie', () => {
-    let trie = new Trie();
-    trie.insert('race');
-    trie.insert('rain');
-    console.log(JSON.stringify(trie, null, 2));
-    expect(trie.root.child.)
-  })
+  it('should have a root', () => {
+    let node = new Node('');
 
-  it('should not insert a word twice' () => {
+    expect(trie.root).to.deep.equal(node);
+  });
 
-  })
+  it('should count words inserted into the trie', () => {
+    trie.insert('dog');
+    expect(trie.count).to.equal(1);
+    trie.insert('cat');
+    expect(trie.count).to.equal(2);
+  });
 
-  it('should find children nodes' () => {
+  it('should not insert a word twice', () => {
+    trie.insert('cake');
+    expect(trie.count).to.equal(1);
+    trie.insert('cake');
+    expect(trie.count).to.equal(1);   
+  });
 
-  })
-
-  it('should ')
-
+  it('should be able to insert a word and the root should have children',
+    () => {
+      trie.insert('apple');
+      expect(trie.root.children.a.letter).to.be.equal('a');
+      expect(trie.root.children.a.children.p.letter).to.equal('p');
+    });
 });
 
 describe('Suggest', () => {
 
+  it('should be a method', () => {
+    expect(trie.suggest).to.be.a('function');
+  });
+
+  it('should return an array', () => {
+    expect(trie.suggest('')).to.deep.equal([]);
+  });
+
+  it('should suggest words from the dictionary', () => {
+    trie.populate(dictionary);
+    trie.insert('pizz');
+    expect(trie.suggest('pizz')).to.include.members(['pizza', 'pizzeria']);
+  });
+
   it('should suggest a word when given a letter', () => {
-    let trie = new Trie();
-        trie.insert('pizza');
-        trie.insert('apple');
-        trie.insert('appeal');
+    trie.insert('pizza');
+    trie.insert('apple');
+    trie.insert('appeal');
     expect(trie.suggest('piz')).to.deep.equal(['pizza']);
-  })
+  });
 
   it('should not be case sensitive', () => {
+    trie.insert('cheese');
+    trie.insert('Cheesy');
+    expect(trie.suggest('CHEE')).to.deep.equal( [ 'cheese', 'cheesy' ] );
+  });
 
-  trie.insert('cheese');
-  trie.insert('cheesy');
-
-  assert.deepEqual(trie.suggest('cheeS'),
-  [ 'cheese', 'cheesy' ] );
+  it('should not suggest words if the word does not exist', () => {
+    trie.populate(dictionary);
+    trie.insert('xj');
+    expect(trie.suggest('xj')).to.deep.equal([]);
   });
 });
 
 describe('Populate', () => {
 
-  it('should populate a dictionary', () => {
-    let trie = new Trie();
-        trie.populate(dictionary);
-    expect(trie.count).to.equal(235886)
-  })
+  it('should be a method', () => {
+    expect(trie.populate).to.be.a('function');
+  });
 
+  it('should populate a dictionary', () => {
+    trie.populate(dictionary);
+    expect(trie.count).to.equal(234371);
+  });
+
+});
+
+describe('Count', () => {
+  it('should return 0 by default', () => {
+    expect(trie.count).to.equal(0);
+  });
+
+  it('should return the word count', () => {
+    trie.insert('pop');
+    trie.insert('pie');
+    expect(trie.count).to.equal(2);
+  });
 });
 
 describe('Select', () => {
 
-  it('should increment word influence every time a word is selected', () => {
-    var trie = new Trie();
-        trie.insert('pie');
-        expect(trie.root.children.p.children.i.children.e.frequency).to.equal(0);
-        trie.select('pie');
-        expect(trie.root.children.p.children.i.children.e.frequency).to.equal(1);
-        trie.select('pie');
-        expect(trie.root.children.p.children.i.children.e.frequency).to.equal(2)
-  })
+  it('should be a method', () => {
+    expect(trie.select).to.be.a('function');
+  });
+
+  it('should increment word frequency every time a word is selected', () => {
+    trie.insert('pie');
+    expect(trie.root.children.p.children.i.children.e.frequency).to.equal(0);
+    trie.select('pie');
+    expect(trie.root.children.p.children.i.children.e.frequency).to.equal(1);
+    trie.select('pie');
+    expect(trie.root.children.p.children.i.children.e.frequency).to.equal(2);
+  });
 
 });
 
